c5cf51673cf0e199d0e2f8478c822ab7f1909ee1
Timothy Reidy <tim.reidy@wsu.edu>
2023-09-22 11:57:54 -0700

    Created basic login/logout flow, login redirects to home page, logout redirects to login page

--- WebDenial/WebDenial/urls.py
+++ WebDenial/WebDenial/urls.py
@@ -19,5 +19,17 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("MessageBoard/", include("MessageBoard.urls")),
+    path("", include("MessageBoard.urls")),
+
+    # By including auths.urls, we automatically get these urls
+    # accounts/login/ [name='login']
+    # accounts/logout/ [name='logout']
+    # accounts/password_change/ [name='password_change']
+    # accounts/password_change/done/ [name='password_change_done']
+    # accounts/password_reset/ [name='password_reset']
+    # accounts/password_reset/done/ [name='password_reset_done']
+    # accounts/reset/<uidb64>/<token>/ [name='password_reset_confirm']
+    # accounts/reset/done/ [name='password_reset_complete']
+    
+    path("accounts/", include("django.contrib.auth.urls")),
 ]
