3ff72c7763c22d72aac64e3e010326a60c73c918
Trent1048 <trentbultsma@gmail.com>
2023-09-22 11:57:05 -0700

    start creating the data models

--- WebDenial/MessageBoard/models.py
+++ WebDenial/MessageBoard/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.PROTECT,
+    )
+
+    def __str__(self):
+        return self.user.get_username()
+    
+class Post(models.Model):
+    class Meta:
+        ordering = ["-publish_date"]
+
+    text = models.TextField()
+    date_created = models.DateTimeField(auto_now_add=True)
+    publish_date = models.DateTimeField(blank=True, null=True)
+    published = models.BooleanField(default=False)
+    author = models.ForeignKey(Profile, on_delete=models.PROTECT)
+
+# TODO keep working on this guy
+# https://realpython.com/python-django-blog/#create-the-django-blog-application
